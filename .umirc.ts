0e453be8f625c8e3965f093f9b53bd3228e12a9d
倏昱 <yufan.yyf@antgroup.com>
2022-04-20 17:36:38 +0800

    💚 ci: fix

--- .umirc.ts
+++ .umirc.ts
@@ -5,6 +5,9 @@ const resolve = (dir: string): string => path.resolve(__dirname, dir);
 
 // more config: https://d.umijs.org/zh-CN/config
 export default defineConfig({
+  base: 'canisminor-ui',
+  publicPath: 'canisminor-ui',
+  exportStatic: {},
   title: 'canisminor-ui',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
