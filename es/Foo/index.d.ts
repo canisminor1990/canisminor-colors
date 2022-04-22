4812747244d8a4598ad423fb9d2b527a94622c26
canisminor1990 <i@canisminor.cc>
2022-04-22 16:31:30 +0800

    🐛 fix: tsconfig.json

--- es/Foo/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: ({ title }: { title: string }) => JSX.Element;
-export default _default;
--- es/Scales/Metals.d.ts
+++
@@ -1,7 +0,0 @@
-import { IColorScale } from './index';
-export declare const Gold: IColorScale;
-export declare const Bronze: IColorScale;
-export declare const Metals: {
-  Gold: IColorScale;
-  Bronze: IColorScale;
-};
