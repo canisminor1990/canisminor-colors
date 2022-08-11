373752dddb7159353fa697ea4e96dad9189f6c7b
倏昱 <yufan.yyf@antgroup.com>
2022-08-11 16:09:17 +0800

    🐛 fix: deps

--- .umirc.ts
+++ .umirc.ts
@@ -13,11 +13,4 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  alias: {
-    '@material/material-color-utilities': resolve('packages/material-color-utilities/'),
-  },
-  library: {
-    // 以下是 father-build（用于组件打包）的配置项，请访问：https://github.com/umijs/father#config
-    esm: 'babel',
-  },
 });
